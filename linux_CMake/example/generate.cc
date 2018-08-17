2d695b672a1c906004b9b7365b5e474d7bd29c2a
zput <wkzxc@sina.cn>
2018-08-17 16:20:40 +0800

    OK
    使用cmake编译成功.

---
+++ linux_CMake/example/generate.cc
@@ -0,0 +1,40 @@
+
+
+#include <cstdio>
+
+#include "Writer.h" 
+#include "FileWriteStream.h"
+
+#if defined _WIN32
+#pragma warning(disable:4996)
+#endif
+
+using namespace json;
+
+int main()
+
+{
+    FileWriteStream os(stdout);
+    Writer<FileWriteStream> writer(os);
+
+	//   stdout.put("----\n");
+	//std::string sssj = "蛤蛤蛤";
+	//std::string_view str1("\0姐姐\0"sv);
+    //std::string sssj = "蛤蛤蛤";
+	//std::string_view str2("姐姐"sv);
+
+	printf("%d\n", INFINITY == INFINITY);
+
+    writer.StartArray();
+    writer.Double(INFINITY);
+    writer.Double(NAN);
+    writer.Bool(true);
+    //writer.String("\0蛤蛤蛤\0"sv);
+	writer.String("\0\xB8\xF2\0"sv);
+	writer.String(""sv);
+    writer.Bool(true);
+    writer.EndArray();
+
+	getchar();
+	return 0;
+}
---
+++ linux_CMake/zxc_json/Value.cc
@@ -0,0 +1,159 @@
+
+
+#include "Value.h"
+
+using namespace zxc_json;
+
+Value::Value(const zxc_json::Value& rhs)
+        : type_(rhs.type_)
+        , a_(rhs.a_)
+{
+    switch (type_) {
+        case TYPE_NULL:
+        case TYPE_BOOL:
+        case TYPE_INT32:
+        case TYPE_INT64:
+        case TYPE_DOUBLE: break;
+        case TYPE_STRING:
+            s_->incrAndGet(); break;
+        case TYPE_ARRAY:
+            a_->incrAndGet(); break;
+        case TYPE_OBJECT:
+            o_->incrAndGet(); break;
+        default: assert(false && "bad value type");
+    }
+}
+
+Value::Value(Value&& rhs)
+        : type_(rhs.type_)
+        , a_(rhs.a_)
+{
+    rhs.type_ = TYPE_NULL;
+    rhs.a_ = nullptr;
+}
+
+Value& Value::operator=(const zxc_json::Value& rhs)
+{
+    assert(this != &rhs);
+    this->~Value();
+    type_ = rhs.type_;
+    a_ = rhs.a_;
+    switch (type_)
+    {
+        case TYPE_NULL:
+        case TYPE_BOOL:
+        case TYPE_INT32:
+        case TYPE_INT64:
+        case TYPE_DOUBLE: break;
+        case TYPE_STRING:
+            s_->incrAndGet(); break;
+        case TYPE_ARRAY:
+            a_->incrAndGet(); break;
+        case TYPE_OBJECT:
+            o_->incrAndGet(); break;
+        default: assert(false && "bad value type");
+    }
+    return *this;
+}
+
+Value& Value::operator=(Value&& rhs)
+{
+    assert(this != &rhs);
+    this->~Value();
+    type_ = rhs.type_;
+    a_ = rhs.a_;
+    rhs.type_ = TYPE_NULL;
+    rhs.a_ = nullptr;
+    return *this;
+}
+
+Value::Value(ValueType type)
+        : type_(type)
+        , s_(nullptr)
+{
+    switch (type_) {
+        case TYPE_NULL:
+        case TYPE_BOOL:
+        case TYPE_INT32:
+        case TYPE_INT64:
+        case TYPE_DOUBLE:                                break;
+        case TYPE_STRING: s_ = new StringWithRefCount(); break;
+        case TYPE_ARRAY:  a_ = new ArrayWithRefCount();  break;
+        case TYPE_OBJECT: o_ = new ObjectWithRefCount(); break;
+        default: assert(false && "bad value type");
+    }
+}
+
+Value::~Value()
+{
+    switch (type_) {
+        case TYPE_NULL:
+        case TYPE_BOOL:
+        case TYPE_INT32:
+        case TYPE_INT64:
+        case TYPE_DOUBLE: break;
+        case TYPE_STRING:
+            if (s_->decrAndGet() == 0)
+                delete s_;
+            break;
+        case TYPE_ARRAY:
+            if (a_->decrAndGet() == 0)
+                delete a_;
+            break;
+        case TYPE_OBJECT:
+            if (o_->decrAndGet() == 0)
+                delete o_;
+            break;
+        default: assert(false && "bad value type");
+    }
+}
+
+Value& Value::operator[] (std::string_view key)
+{
+    assert(type_ == TYPE_OBJECT);
+
+    auto it = findMember(key);
+    if (it != o_->data.end())
+        return it->value;
+
+    assert(false); // unlike std::map
+    static Value fake(TYPE_NULL);
+    return fake;
+}
+
+size_t Value::getSize() const
+{
+    if (type_ == TYPE_ARRAY)
+        return a_->data.size();
+    else if (type_ == TYPE_OBJECT)
+        return o_->data.size();
+    return 1;
+}
+
+const Value&  Value::operator[] (std::string_view key) const
+{
+    return const_cast<Value&>(*this)[key];
+}
+
+Value::MemberIterator Value::findMember(std::string_view key)
+{
+    assert(type_ == TYPE_OBJECT);
+    return std::find_if(o_->data.begin(), o_->data.end(), [key](const Member& m)->bool {
+        return m.key.getStringView() == key;
+    });
+}
+
+Value::ConstMemberIterator Value::findMember(std::string_view key) const
+{
+    return const_cast<Value&>(*this).findMember(key);
+}
+
+
+Value& Value::addMember(Value&& key, Value&& value)
+{
+    assert(type_ == TYPE_OBJECT);
+    assert(key.type_ == TYPE_STRING);
+    assert(findMember(key.getStringView()) == memberEnd());
+    o_->data.emplace_back(std::move(key), std::move(value));
+    return o_->data.back().value;
+}
